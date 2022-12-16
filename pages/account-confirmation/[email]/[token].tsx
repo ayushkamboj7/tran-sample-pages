fbba80a2a1c93ef05712431b8e8d59662fc14d0a
Ayush Kamboj <aayush.kamboj@stackera.io>
2022-12-16 13:49:50 +0530

    Tran sample pages

---
+++ pages/account-confirmation/[email]/[token].tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+export default function Home() {
+  const router = useRouter();
+  const token = router.query.token;
+  const email = router.query.email;
+
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    let data = JSON.stringify({
+      email: email,
+      token: token,
+    });
+
+    let config = {
+      method: "post",
+      url: "https://traninnovation.com/api/account/confirmation",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response: any) {
+        setMessage(response.data.msg);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [router.isReady]);
+  if (message) {
+    return (
+      <section className=" flex justify-center flex-col items-center bg-[#449eff] h-screen">
+        <div className=" mt-[100px] flex flex-col items-center">
+          <h2 className="text-[#fff] text-[1.5rem] text-center">{message}</h2>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className=" flex justify-center flex-col items-center bg-[#449eff] h-screen">
+        <div className=" mt-[100px] flex flex-col items-center">
+          <h2 className="text-[#fff] text-[1.5rem] text-center">
+            Something went wrong !
+          </h2>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ pages/forgotPassword/[token].tsx
@@ -0,0 +1,67 @@
+import { useRouter } from "next/router";
+import axios from "axios";
+
+const formSubmitted = (event: any, token: any) => {
+  event.preventDefault();
+  var data = JSON.stringify({
+    newPassword: event.target.password.value,
+  });
+
+  var config = {
+    method: "post",
+    url: `https://traninnovation.com/api/reset/password/${token}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      if (response.data === "success") {
+        alert("Password Changed");
+      } else {
+        alert(response.data.msg);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+export default function ForgotPassword() {
+  const router = useRouter();
+  return (
+    <section className=" flex justify-center flex-col items-center bg-[#449eff] h-screen">
+      <div className=" mt-[100px] flex flex-col items-center">
+        <h2 className="text-[#fff] text-[1.5rem]  text-center">
+          Forgot Password
+        </h2>
+      </div>
+      <form
+        onSubmit={(event) => formSubmitted(event, router.query.token)}
+        className="mt-[30px] mb-[15px]"
+      >
+        <input
+          className="block w-[350px] h-[50px] outline-none my-[25px] pl-4 rounded-md"
+          type="password"
+          placeholder="Enter Password"
+          required
+          name="password"
+        />
+
+        <input
+          className="block w-[350px] h-[50px] outline-none my-[25px] pl-4 rounded-md"
+          type="password"
+          placeholder="Enter password again "
+          required
+          name="password-again"
+        />
+
+        <button className="block bg-[#fff] text-[#449eff] px-[40px] py-[10px] mt-[30px] rounded mx-auto">
+          Reset Password
+        </button>
+      </form>
+    </section>
+  );
+}
